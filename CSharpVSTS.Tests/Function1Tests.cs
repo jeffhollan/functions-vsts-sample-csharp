ec7479b8d6e776ca294a3ee11e655f3ed22de917
Jeff Hollan <jehollan@microsoft.com>
2018-07-11 14:17:46 -0700

    adding unit tests

---
+++ CSharpVSTS.Tests/Function1Tests.cs
@@ -0,0 +1,28 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Internal;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
+using System;
+using Xunit;
+
+namespace CSharpVSTS.Tests
+{
+    public class Function1Test
+    {
+        [Fact]
+        public void CallHttp()
+        {
+            HttpContext context = new DefaultHttpContext();
+            HttpRequest request = new DefaultHttpRequest(context);
+            request.Method = "GET";
+            request.Body = null;
+
+            ILogger nullLogger = new NullLogger<string>();
+            var response = Function1.Run(request, nullLogger) as OkObjectResult;
+
+            Assert.NotNull(response);
+            Assert.Equal(200, response.StatusCode);
+        }
+    }
+}
